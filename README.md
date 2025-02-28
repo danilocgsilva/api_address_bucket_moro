b77e30f7070064d2b3385c3a926d30173e8a6175
Danilo Silva <contact@danilocgsilva.me>
2025-02-28 18:31:55 -0300

    Renamed folder, starting query term

---
+++ README.md
@@ -0,0 +1,7 @@
+# API ADDRESS BUCKET MORO
+
+Store endpoints from apis.
+
+## To develop
+
+The aplication is beign developed using docker. There are two container: one for application and another for the database. Both have the notwork mode to *host*, so it is good to interates to the development server for frontend.
---
+++ application/database/migrations/2025_02_28_212449_create_query_strings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('query_strings', function (Blueprint $table) {
+            $table->id();
+            $table->string("term");
+            $table->bigInteger("api_id")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('query_strings');
+    }
+};
