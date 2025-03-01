62006060c053e124b190f7720a7a56c884d64ade
Danilo Silva <contact@danilocgsilva.me>
2025-03-01 18:28:38 -0300

    Added routes to add query string

--- application/routes/web.php
+++ application/routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QueryStringController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('api', ApiController::class);
+Route::resource("api.querystring", QueryStringController::class);
