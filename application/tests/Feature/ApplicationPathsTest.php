61fb68c561178a433d401a2dff49cc32ca5c7c86
Danilo Silva <contact@danilocgsilva.me>
2025-03-09 11:15:13 -0300

    Added unit test for dependent model

--- application/tests/Feature/ApplicationPathsTest.php
+++ application/tests/Feature/ApplicationPathsTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Api;
+use App\Models\{QueryTerm, QueryStringQueryTerm};
+use DB;
+use Mockery;
 
 class ApplicationPathsTest extends TestCase
 {
@@ -27,5 +29,50 @@ class ApplicationPathsTest extends TestCase
         $response = $this->get('api');
 
         $response->assertStatus(200);
+    }
+
+    public function testAddNewQuery()
+    {
+        $this->prepareAddNewQuery();
+
+        $response = $this->get('api/1/terms/create');
+
+        $response->assertStatus(200);
+    }
+
+    public function testAddNewQueryNotExistingApi()
+    {
+        $this->prepareAddNewQuery();
+
+        $response = $this->get('api/2/terms/create');
+
+        $response->assertStatus(404);
+    }
+
+    private function prepareAddNewQuery()
+    {
+        $this->truncateData();
+        $this->writeApi();
+    }
+
+    private function truncateData()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        QueryTerm::truncate();
+        QueryStringQueryTerm::truncate();
+        Api::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    private function writeApi()
+    {
+        $api = new Api();
+        $api->name = 'Test API';        
+        $api->address = 'http://localhost:8000';
+        $api->description = 'Test API Description';
+        $api->documentation = 'http://localhost:8000/docs';
+        $api->save();
+
+        return $api;
     }
 }
